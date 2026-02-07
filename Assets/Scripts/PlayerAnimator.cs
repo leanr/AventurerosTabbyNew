a7b5950e41665c3660cbad66169a85044cfea722
Tabby <leandro.rozan@gmail.com>
2026-02-07 14:25:54 +0100

    animacions de mage

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -11,12 +11,32 @@ public class PlayerAnimator : MonoBehaviour
 
     public void SetRunning(bool isRunning)
     {
-        //animator.SetBool("IsRunning", isRunning);
+        animator.SetBool("IsRunning", isRunning);
     }
 
     public void PlayHit()
     {
         animator.ResetTrigger("Hit");
         animator.SetTrigger("Hit");
+    }
+
+    public void RecieveHit()
+    {
+        animator.ResetTrigger("HitAventurero");
+        animator.SetTrigger("HitAventurero");
+    }
+
+    public void SetDeath()
+    {
+        animator.ResetTrigger("Death");
+        animator.SetTrigger("Death");
+    }
+
+    public void StopMovementAnimations()
+    {
+        // Reinicia cualquier trigger o bool que active correr o hit
+        animator.ResetTrigger("Hit");
+        animator.ResetTrigger("HitAventurero");
+        animator.SetBool("IsRunning", false);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,25 @@ public class PlayerController : MonoBehaviour
     Movement movement;
     PlayerAnimator playerAnimator;
     bool isHit;
+    bool isHitAventurero = false;
+    bool playerIsDead = false;
+
+
+    [Header("Vida")]
+    public int maxHealth = 100;
+    int currentHealth;
 
     void Awake()
     {
         movement = GetComponent<Movement>();
         playerAnimator = GetComponent<PlayerAnimator>();
+        currentHealth = maxHealth;
     }
 
     void Update()
     {
         // Solo actualiza run si NO está en hit
-        if (!isHit)
+        if(!isHit && !playerIsDead)
         {
             playerAnimator.SetRunning(movement.canMove);
         }
@@ -23,6 +31,7 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+
         if (collision.gameObject.CompareTag("SkeletonRogue"))
 
 
@@ -31,8 +40,23 @@ public class PlayerController : MonoBehaviour
             movement.canMove = false;
             print("estoy chocando");
             playerAnimator.PlayHit();
+       
 
-            
+
+        }
+
+
+        if (collision.gameObject.CompareTag("SkeletonRogue"))
+
+
+        {
+            isHit = true;
+            movement.canMove = false;
+            print("estoy chocando");
+            playerAnimator.PlayHit();
+    
+
+
         }
 
         if (collision.gameObject.CompareTag("SkeletonMinion"))
@@ -43,7 +67,7 @@ public class PlayerController : MonoBehaviour
             movement.canMove = false;
             print("estoy chocando con el otro enemigo");
             playerAnimator.PlayHit();
-
+           
 
         }
 
@@ -55,7 +79,18 @@ public class PlayerController : MonoBehaviour
             movement.canMove = false;
             print("estoy chocando con el otro enemigo");
             playerAnimator.PlayHit();
+      
 
+        }
+
+        if (collision.gameObject.CompareTag("Arrow"))
+
+
+        {
+           // isHitAventurero = true;
+           // movement.canMove = false;
+            print("me dio la flecha");
+            TakeDamage(50); // daño de 1 por golpe
 
         }
     }
@@ -63,9 +98,20 @@ public class PlayerController : MonoBehaviour
     // Llamado por Animation Event al final del hit
     public void OnHitFinished()
     {
-        print("vuelvo a caminar");
+        if (playerIsDead) return;
+        print("vuelvo a caminar despues de golpear");
         isHit = false;
         movement.canMove = true;
+        playerAnimator.SetRunning(movement.canMove);
+    }
+
+    public void RecieveHitFinished()
+    {
+        if (playerIsDead) return;
+        print("vuelvo a caminar despues de recibir golpe");
+        isHitAventurero = false;
+        movement.canMove = true;
+        playerAnimator.SetRunning(movement.canMove);
     }
 
     public void DoAttack()
@@ -107,5 +153,45 @@ public class PlayerController : MonoBehaviour
         
 
 
+    }
+
+    public void TakeDamage(int damage)
+    {
+       // if (isHitAventurero) return; // evita daño múltiple en el mismo golpe
+
+        currentHealth -= damage;
+        Debug.Log("Vida restante: " + currentHealth);
+
+        isHitAventurero = true;
+        movement.canMove = false;
+        playerAnimator.RecieveHit();
+
+
+        if (currentHealth <= 0)
+        {
+            Die();
+            print("empiezo la muerte");
+           // Destroy(gameObject, 1f);
+
+        }
+    }
+
+    void Die()
+    {
+        playerIsDead = true;                   // bloquea todo
+        movement.canMove = false;         // asegurate de que no pueda moverse
+         playerAnimator.StopMovementAnimations();
+        playerAnimator.SetDeath();        // dispara animación de muerte
+       
+    }
+
+    public void DeathFinished()
+    {
+        Debug.Log("El jugador murió");
+       // movement.canMove = false;
+        Destroy(gameObject);
+
+
+        // acá después podemos poner animación de muerte o respawn
     }
 }
